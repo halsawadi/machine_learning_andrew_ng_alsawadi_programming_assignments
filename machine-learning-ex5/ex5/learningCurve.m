94d798fccdc8886e1be8c52693370ae936b1b895
Hassan Alsawadi <h.alsawaddi@gmail.com>
2020-01-07 19:35:39 +0300

    Completed successfully Programming Assignment 5.

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -16,6 +16,8 @@ function [error_train, error_val] = ...
 
 % Number of training examples
 m = size(X, 1);
+% Number of Cross-validation examples
+m_cv = size(Xval, 1);
 
 % You need to return these values correctly
 error_train = zeros(m, 1);
@@ -52,7 +54,11 @@ error_val   = zeros(m, 1);
 %
 
 % ---------------------- Sample Solution ----------------------
-
+for i = 1:m
+    theta = trainLinearReg(X(1:i,:), y(1:i), lambda);
+    [error_train(i), ~] = linearRegCostFunction(X(1:i,:), y(1:i), theta, 0);
+    [error_val(i), ~] = linearRegCostFunction(Xval, yval, theta, 0);
+end
 
 
 
--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -18,12 +18,15 @@ grad = zeros(size(theta));
 %
 %               You should set J to the cost and grad to the gradient.
 %
+h_x = X*theta;
+seq_err_func = (h_x - y).^2;
+reg_pnlty = theta(2:end).^2;
 
+J = (1/(2*m))*sum(seq_err_func) + (lambda/(2*m))*sum(reg_pnlty);
 
+grad(1) = (1/m)*sum(((h_x - y).*X(:,1)));
 
-
-
-
+grad(2:end) = (1/m)*sum((h_x - y).*X(:,2:end))' + (lambda/m)*theta(2:end);
 
 
 
